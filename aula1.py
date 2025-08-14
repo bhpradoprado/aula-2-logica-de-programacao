b3328d72a48da00692c6bbe19331089b67f2d0dc
Renan Raad <bhpradoprado@gmail.com>
2025-08-14 16:32:17 -0300

    revisão aula 1, algoritimos

--- aula1.py
+++ aula1.py
@@ -1,1 +1,22 @@
-print('hello world')
+# nome = "larissa"
+# idade = 48
+
+inputnome = input('Qual seu nome?')
+inputidade = int(input('Qual sua idade'))
+
+#int, double, float
+
+
+#Booleanos
+pessoas = True
+sala = False
+
+
+#mostrar no terminal
+print(inputnome)
+print(inputidade)
+
+#os dois juntos
+print(inputnome,inputidade)
+
+print(f'esse é o nome colocado: {inputnome} e a idade é {inputidade}')
